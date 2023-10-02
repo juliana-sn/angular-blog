54a6fe48e2a7167b8f40558eddf9b6d86ce91f00
Juliana Silva <ju_snascimento@outlook.com>
2023-10-01 22:06:05 -0300

    conteúdo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake =[
+  {
+    "id":"1",
+    "image": "https://businessmirror.com.ph/wp-content/uploads/2023/08/D_Moving-Group-Poster-Horizontal.jpg",
+    "title": "DORAMA MOVING FAZ SUCESSO",
+    "text": "O novo dorama Moving do Star+ ficou entre os doramas mais assistidos"
+  },
+  {
+    "id":"2",
+    "image": "https://cinepop.com.br/wp-content/uploads/2022/09/FcUPBihaMAAhlM1.jpg",
+    "title": "Segunda temporada Loki",
+    "text": "Chegou no streaming Disney+ a segunda temporada de Loki"
+  },
+  {
+    "id":"3",
+    "image": "https://tm.ibxk.com.br/2023/09/28/28083927576658.jpg?ims=164x118",
+    "title": "Netflix anuncia terceira temporada de Alice in Borderland",
+    "text": "Netflix anunciou esse semana a terceira temporada de Alice in Borderland, levando os fãs da série a loucura com teorias"
+  },
+  {
+    "id":"4",
+    "image": "https://tm.ibxk.com.br/2023/09/26/26122814303166.jpg?ims=164x118",
+    "title": "Novo filme do Homem de Ferro anunciado",
+    "text": "meu sonho que o Robert Downey Jr voltasse"
+  }
+]
